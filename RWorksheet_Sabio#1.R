95536b2e0461cc53082bdaa69b4a581d7405f28a
Lore Faith Sabio <lorefaithsabio25792gmail.com>
2023-09-20 02:19:10 +0000

    worksheet commit

---
+++ RWorksheet_Sabio#1.R
@@ -0,0 +1,93 @@
+#1. Set up vector named age.
+numberVector <- c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29,
+    35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 42, 53, 41,
+    51, 35, 24, 33, 41)
+
+#data points
+length(numberVector) 
+
+#codes
+seq(numberVector)
+
+min(numberVector)
+
+max(numberVector)
+
+c(numberVector)
+
+sort(numberVector)
+
+sum(numberVector)
+
+#2. Find the reciprocal of the values for age. Write the code and its output.
+recip_age <-1/numberVector
+recip_age
+
+#3.Assign also new_age <- c(age, 0, age). 
+assign<-c(17,0,34)
+
+#4.Sort the values of age
+sort(numberVector)
+sort(recip_age)
+
+#5.The minimum and maximum value for age.
+max(numberVector)
+min(numberVector)
+max(recip_age)
+min(recip_age)
+
+#6. vector named data.
+numberData <- c(2.4, 2.8, 2.1, 2.5, 2.4, 2.2, 2.5, 2.3,
+                2.5, 2.3, 2.4, 2.7)
+#data points
+length(numberData)
+
+#codes
+seq(numberData)
+
+min(numberData)
+
+max(numberData)
+
+c(numberData)
+
+sort(numberData)
+
+sum(numberData)
+
+#7.double data
+dob_data <- 2*numberData
+dob_data
+
+#8.sequence
+#int(1-100)
+seq(1,100)
+
+# numbers (20-60)
+seq (20,60,5)
+
+#mean numbers (20-60)
+mean(20,60)
+
+#sum of (51-91)
+seq(51,91)
+sum(seq)
+
+#int(1-1000)
+seq(1,1000)
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+  
+
